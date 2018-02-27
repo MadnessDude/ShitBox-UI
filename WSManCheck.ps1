f1c10b1e65b04b7251004b7947c3a27193e8a94c
MicroDudeNor <fastsignup@gmail.com>
2018-02-27 14:16:02 +0100

    WSMan Check
    Code is impartial. Check if IF Statements are conflicting?

---
+++ WSManCheck.ps1
@@ -0,0 +1,18 @@
+﻿# Web Services for Management (WSMAN) Checker
+# Added 27.02.18
+# Checks if WSMan is running, if not start service
+# Code is impartial, if statements conflicting??
+
+function FuncCheckService{
+ param($ServiceName)
+ $CheckWSMAN = Get-Service -Name $WSMan
+ if ($CheckWSMAN.Status -ne "running"){
+ Start-Service $ServiceName
+ Write-Host "Starting WSMan service" 
+ " ---------------------- " 
+ " Service is now started"
+ }
+ if ($CheckWSMAN.Status -eq "running"){ 
+ Write-Host "$WSMan service is already started"
+ }
+ }
