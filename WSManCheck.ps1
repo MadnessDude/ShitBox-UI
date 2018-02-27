0a0267a82eb51b23a5faa4ae13bb095991961a74
MadnessDude <20458103+MadnessDude@users.noreply.github.com>
2018-02-27 16:54:53 +0100

    Updated WSManCheck for shitty code.
    Added Admin prompt, as you need to be admin to enable WSMan / WinRM

--- WSManCheck.ps1
+++ WSManCheck.ps1
@@ -4,15 +4,20 @@
 # Code is impartial, if statements conflicting??
 
 function FuncCheckService{
- param($ServiceName)
- $CheckWSMAN = Get-Service -Name $WSMan
- if ($CheckWSMAN.Status -ne "running"){
- Start-Service $ServiceName
- Write-Host "Starting WSMan service" 
- " ---------------------- " 
- " Service is now started"
- }
- if ($CheckWSMAN.Status -eq "running"){ 
- Write-Host "$WSMan service is already started"
- }
+
+ $CheckWSMAN = Get-Service -Name WinRM
+
+    if ($CheckWSMAN.Status -ne "running"){
+        # Checking if you are running this shell as admin.
+        $AreYouAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+
+        if ($AreYouAdmin -match 'False') {Write-Host "You need to run as admin bro." ; break}
+        else {Start-Service WinRM ; Write-Host "Starting WinRM-Service"}
+    }
+
+    elseif ($CheckWSMAN.Status -eq "running"){ 
+    Write-Host "$WSMan service is already started"
+    }
+
+    else{Write-Error "Something went wrong."}
  }
